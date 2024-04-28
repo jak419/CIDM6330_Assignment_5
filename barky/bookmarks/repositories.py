5656f07bab000f007e2b0d8d54f714aeb55807f1
John A <johnanokye94@yahoo.com>
2024-04-27 21:25:04 -0600

    adding injector dependency and setting up redis

--- barky/bookmarks/repositories.py
+++ barky/bookmarks/repositories.py
@@ -1,12 +1,33 @@
-from .models import Bookmark
+import pytz
+from datetime import datetime
 from django.db import transaction
+from injector import Injector, inject, Module, provider, singleton
+from .models import Bookmark
 
+# class definition for TimeStampProvider 
+class TimeStampProvider:
+    def get_current_time(self):
+        return datetime.now(pytz.UTC)
+
+# class definition for Injector module definition
+class AppModule(Module):
+    @singleton
+    @provider
+    def provide_timestamp_provider(self) -> TimeStampProvider:
+        return TimeStampProvider()
+
+# BookmarkRepository class definition with dependency injection
 class BookmarkRepository:
+    @inject
+    def __init__(self, timestamp_provider: TimeStampProvider):
+        self.timestamp_provider = timestamp_provider
+
     def get_all_bookmarks(self):
         return Bookmark.objects.all()
 
     def add_bookmark(self, title, url, notes=''):
-        return Bookmark.objects.create(title=title, url=url, notes=notes)
+        timestamp = self.timestamp_provider.get_current_time()
+        return Bookmark.objects.create(title=title, url=url, notes=notes, date_added=timestamp)
 
     def get_bookmark_by_id(self, id):
         return Bookmark.objects.filter(id=id).first()
@@ -15,7 +36,12 @@ class BookmarkRepository:
         Bookmark.objects.filter(id=id).update(**kwargs)
 
     def delete_bookmark(self, id):
-         with transaction.atomic():
+        with transaction.atomic():
             bookmark = self.get_bookmark_by_id(id)
             if bookmark:
                 bookmark.delete()
+
+
+injector = Injector([AppModule()]) #setting up injector
+bookmark_repository = injector.get(BookmarkRepository) #setting up bookmark_repository to be used in views.py
+
--- barky/bookmarks/views.py
+++ barky/bookmarks/views.py
@@ -6,6 +6,7 @@ from .repositories import BookmarkRepository
 from django.shortcuts import redirect
 from django.views import View
 from .models import Bookmark
+from .repositories import bookmark_repository 
 
 class BookmarkListView(ListView):
     model = Bookmark
@@ -13,18 +14,20 @@ class BookmarkListView(ListView):
     paginate_by = 10
 
     def get_queryset(self):
-        repository = BookmarkRepository()
-        return repository.get_all_bookmarks()
+        #repository = BookmarkRepository()
+        #return repository.get_all_bookmarks()
+        return bookmark_repository.get_all_bookmarks()
 
 
 class BookmarkAddView(View):
     def post(self, request, *args, **kwargs):
-        # Assuming you have title, url, and notes from the request.POST
+        #  having title, url, and notes from the request.POST
         title = request.POST.get('title')
         url = request.POST.get('url')
         notes = request.POST.get('notes', '')
         
-        repository = BookmarkRepository()
-        repository.add_bookmark(title, url, notes)
+        #repository = BookmarkRepository()
+        #repository.add_bookmark(title, url, notes)
+        bookmark_repository.add_bookmark(title, url, notes)
 
         return redirect('bookmark-list')
